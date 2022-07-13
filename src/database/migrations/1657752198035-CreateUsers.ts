4f590bf52cfcf33927b22d3c51e738a4c46b11d8
Tiago Araujo <tiagoaraujosud@gmail.com>
2022-07-13 19:53:08 -0300

    Create migrations path

---
+++ src/database/migrations/1657752198035-CreateUsers.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class CreateUsers1657752198035 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
