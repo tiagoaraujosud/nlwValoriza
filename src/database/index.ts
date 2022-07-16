d89466d92595d069642bf81567284aaabfc07cd8
Tiago Araujo <tiagoaraujosud@gmail.com>
2022-07-16 07:28:53 -0300

    Change Typeorm version and configurations

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,14 +1,3 @@
-import { DataSource } from "typeorm";
+import { createConnection } from "typeorm";
 
-const myConnection = new DataSource({
-    type: "sqlite",
-    database: "src/database/database.sqlite",
-})
-
-myConnection.initialize()
-    .then(() => {
-        console.log("Data Source has been initialized!")
-    })
-    .catch((err) => {
-        console.error("Error during Data Source initialization", err)
-    })
+createConnection();
--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,6 @@ import express, { request, response } from "express";
 
 import "./database";
 
-
 const app = express();
 
 app.listen(3000, () => console.log("Server is running"));
