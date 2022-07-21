4c30bec1af6067e7bdf8bb463b3ae7f11e1b724c
Tiago Araujo <tiagoaraujosud@gmail.com>
2022-07-21 10:00:50 -0300

    Refactor: Add password

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -4,12 +4,13 @@ import { UsersRepositories } from "../repositories/UsersRepositories";
 interface IUserRequest{
     name: string;
     email: string;
-    admin?: boolean
+    admin?: boolean;
+    password: string;
 }
 
 
 class CreateUserService{
-    async execute({ name, email, admin }: IUserRequest ){
+    async execute({ name, email, admin, password }: IUserRequest ){
 
         const usersRepository = getCustomRepository(UsersRepositories);
 
@@ -28,7 +29,8 @@ class CreateUserService{
         const user = usersRepository.create({
             name,
             email, 
-            admin
+            admin,
+            password
         })
 
         await usersRepository.save(user);
