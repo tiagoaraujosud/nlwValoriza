44b0ba67db59e35c8ee487d27c41abea0702dc96
Tiago Araujo <tiagoaraujosud@gmail.com>
2022-07-25 19:30:12 -0300

    Refactory: ensureAdmin.ts

--- src/middlewares/ensureAdmin.ts
+++ src/middlewares/ensureAdmin.ts
@@ -1,15 +1,18 @@
 import {Request, Response, NextFunction} from "express"
+import { getCustomRepository } from "typeorm";
+import { UsersRepositories } from "../repositories/UsersRepositories";
 
 
-export function ensureAdmin(request: Request, response: Response, next: NextFunction){
+export async function ensureAdmin(request: Request, response: Response, next: NextFunction){
 
     const { user_id } = request;
 
-    console.log(user_id);
-   
+    const usersRepositories = getCustomRepository(UsersRepositories);
+
+    const { admin } = await usersRepositories.findOne(user_id);
+    
 
     //Verifica se o usuário é admin
-    const admin = true;
 
     if(admin){
         return next();
